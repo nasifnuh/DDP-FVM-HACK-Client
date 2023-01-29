cab8c4148f35e7d53cbc12b33a9c1f29c95d07af
AFPedreros <felipe.pedreros94@gmail.com>
2023-01-29 09:40:57 -0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import "./App.css";
-import "primereact/resources/themes/lara-light-teal/theme.css"; //theme
-import "primereact/resources/primereact.min.css"; //core css
-import "primeicons/primeicons.css"; //icons
+import './App.css';
+import 'primereact/resources/themes/lara-light-teal/theme.css'; //theme
+import 'primereact/resources/primereact.min.css'; //core css
+import 'primeicons/primeicons.css'; //icons
 
-import AuthenticatedRoute from "./components/auth/AuthenticatedRoute";
+import AuthenticatedRoute from './components/auth/AuthenticatedRoute';
 
-import Explore from "./components/pages/Explore";
-import Home from "./components/pages/Home";
-import PageNotFound from "./components/pages/PageNotFound";
-import Profile from "./components/pages/Profile";
-import TaskBoard from "./components/pages/TaskBoard";
+import Explore from './components/pages/Explore';
+import Home from './components/pages/Home';
+import PageNotFound from './components/pages/PageNotFound';
+import Profile from './components/pages/Profile';
+import TaskBoard from './components/pages/TaskBoard';
 
 const App = () => {
-  return (
-    <div>
-      <Router>
-        <Route exact path="/" component={Home} />
-        {/* Remove below 3 routes and use the commented <Switch></Switch> when been made dynamic */}
-        <Route exact path="/explore" component={Explore} />
-        <Route exact path="/profile" component={Profile} />
-        <Route exact path="/board" component={TaskBoard} />
-        {/* <Switch>
+    return (
+        <div>
+            <Router>
+                <Route exact path='/' component={Home} />
+                {/* Remove below 3 routes and use the commented <Switch></Switch> when been made dynamic */}
+                <Route exact path='/explore' component={Explore} />
+                <Route exact path='/profile' component={Profile} />
+                <Route exact path='/board' component={TaskBoard} />
+                {/* <Switch>
           <AuthenticatedRoute
             exact
             path="/"
@@ -49,8 +49,8 @@ const App = () => {
             roles={[USER_DEVELOPER, USER_REVIEWER, USER_TESTER]}
           />
         </Switch> */}
-      </Router>
-    </div>
-  );
+            </Router>
+        </div>
+    );
 };
 export default App;
--- src/components/pages/TaskBoard/Board.jsx
+++ src/components/pages/TaskBoard/Board.jsx
@@ -1,92 +1,98 @@
-import List from "./List";
-import { useState } from "react";
-import { DndContext, rectIntersection } from "@dnd-kit/core";
+import List from './List';
+import { useState } from 'react';
+import { DndContext, rectIntersection } from '@dnd-kit/core';
 
 const todoList = [
-  {
-    name: "Task1",
-    category: "Design",
-    reward: "1 ETH",
-  },
-  {
-    name: "Task2",
-    category: "Frontend",
-    reward: "1 ETH",
-  },
-  {
-    name: "Task3",
-    category: "Backend",
-    reward: "1 ETH",
-  },
+    {
+        name: 'Task1',
+        category: 'Design',
+        reward: '1 ETH',
+    },
+    {
+        name: 'Task2',
+        category: 'Frontend',
+        reward: '1 ETH',
+    },
+    {
+        name: 'Task3',
+        category: 'Backend',
+        reward: '1 ETH',
+    },
 ];
 const progressList = [
-  {
-    name: "Task1",
-    category: "Design",
-    reward: "1 ETH",
-  },
+    {
+        name: 'Task1',
+        category: 'Design',
+        reward: '1 ETH',
+    },
 ];
 const doneList = [
-  {
-    name: "Task1",
-    category: "Design",
-    reward: "1 ETH",
-  },
-  {
-    name: "Task2",
-    category: "Frontend",
-    reward: "1 ETH",
-  },
+    {
+        name: 'Task1',
+        category: 'Design',
+        reward: '1 ETH',
+    },
+    {
+        name: 'Task2',
+        category: 'Frontend',
+        reward: '1 ETH',
+    },
 ];
 
 export default function Board() {
-  const [todoItems, setTodoItems] = useState(todoList);
-  const [inProgressItems, setInProgressItems] = useState(progressList);
-  const [doneItems, setDoneItems] = useState(doneList);
+    const [todoItems, setTodoItems] = useState(todoList);
+    const [inProgressItems, setInProgressItems] = useState(progressList);
+    const [doneItems, setDoneItems] = useState(doneList);
 
-  return (
-    <DndContext
-      collisionDetection={rectIntersection}
-      onDragEnd={(e) => {
+    function handleDragEnd(e) {
         const container = e.over?.id;
         const item = {
-          name: e.active.data.current?.name ?? "",
-          category: e.active.data.current?.category ?? "",
-          reward: e.active.data.current?.reward ?? "",
+            name: e.active.data.current?.name ?? '',
+            category: e.active.data.current?.category ?? '',
+            reward: e.active.data.current?.reward ?? '',
         };
         const index = e.active.data.current?.index ?? 0;
-        const parent = e.active.data.current?.parent ?? "ToDo";
+        const parent = e.active.data.current?.parent ?? 'ToDo';
 
-        if (container === "ToDo") {
-          setTodoItems([...todoItems, item]);
-        } else if (container === "Done") {
-          setDoneItems([...doneItems, item]);
-        } else {
-          setInProgressItems([...inProgressItems, item]);
+        if (parent === container || container === undefined) {
+            return;
         }
-        if (parent === "ToDo") {
-          setTodoItems([
-            ...todoItems.slice(0, index),
-            ...todoItems.slice(index + 1),
-          ]);
-        } else if (parent === "Done") {
-          setDoneItems([
-            ...doneItems.slice(0, index),
-            ...doneItems.slice(index + 1),
-          ]);
+
+        if (container === 'ToDo') {
+            setTodoItems([...todoItems, item]);
+        } else if (container === 'Done') {
+            setDoneItems([...doneItems, item]);
         } else {
-          setInProgressItems([
-            ...inProgressItems.slice(0, index),
-            ...inProgressItems.slice(index + 1),
-          ]);
+            setInProgressItems([...inProgressItems, item]);
         }
-      }}
-    >
-      <div className="flex gap-4 p-8">
-        <List title="ToDo" items={todoItems} />
-        <List title="Progress" items={inProgressItems} />
-        <List title="Done" items={doneItems} />
-      </div>
-    </DndContext>
-  );
+        if (parent === 'ToDo') {
+            setTodoItems([
+                ...todoItems.slice(0, index),
+                ...todoItems.slice(index + 1),
+            ]);
+        } else if (parent === 'Done') {
+            setDoneItems([
+                ...doneItems.slice(0, index),
+                ...doneItems.slice(index + 1),
+            ]);
+        } else {
+            setInProgressItems([
+                ...inProgressItems.slice(0, index),
+                ...inProgressItems.slice(index + 1),
+            ]);
+        }
+    }
+
+    return (
+        <DndContext
+            collisionDetection={rectIntersection}
+            onDragEnd={handleDragEnd}
+        >
+            <div className='flex gap-4 p-8'>
+                <List title='ToDo' items={todoItems} />
+                <List title='Progress' items={inProgressItems} />
+                <List title='Done' items={doneItems} />
+            </div>
+        </DndContext>
+    );
 }
